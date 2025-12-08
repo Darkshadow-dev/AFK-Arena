16e701556a1237a5cabedcf7f6f34ae537108cfb
Darkshadow-dev <ahmedhasid479@gmail.com>
2025-12-08 18:49:14 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBcLsgOSSf2cUNK_9G49L7kSbuzZ2lt0VQ",
+  authDomain: "afk-arena-e8f4a.firebaseapp.com",
+  projectId: "afk-arena-e8f4a",
+  storageBucket: "afk-arena-e8f4a.firebasestorage.app",
+  messagingSenderId: "827652125732",
+  appId: "1:827652125732:web:d8026c1fe271483aaf54b1"
+});
